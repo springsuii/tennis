39e7ed60f83925a2ecc2d5fb7914f78da3e24908
Piyabut Nawanichbumrung <emunity3137@gmail.com>
2016-09-27 16:03:19 +0700

    add function scoreA

--- spec/tennisAB.spec.js
+++ spec/tennisAB.spec.js
@@ -5,25 +5,32 @@ function tennis(){
     ScoreB = 0;
     ScoreA = 0;
   }
+  this.scoreA = function(){
+    if(ScoreA === 0)
+        ScoreA = 15;
+    else if(ScoreA === 0)
+        ScoreA = 30;
+    else if(ScoreA === 0)
+        ScoreA = 40;
+    return ScoreA;
+  }
   this.echoScore = function(){
     if(ScoreA === 0 && ScoreB === 0)
-      return 'Love-Love';
+      return 'Love - Love';
+    else if(ScoreA === 15 && ScoreB === 0)
+      return 'Fifteen - Love';
   }
-  this.echoScore = function(){
-    if(ScoreA === 15 && ScoreB === 0)
-      return 'Fifteen-Love';
-  }
-
 }
 describe('Tennis', function () {
   it('Start Game score 0 - 0 echo Love - Love', function () {
     var ten = new tennis();
     ten.startGame();
-    expect(ten.echoScore()).toEqual('Love-Love');
+    expect(ten.echoScore()).toEqual('Love - Love');
   });
-  it('check score A 15 - B 0 echo fifteen - love',function()){
+  it('check score A 15 - B 0 echo fifteen - love',function(){
     var ten = new tennis();
+    ten.startGame();
     ten.scoreA();
     expect(ten.echoScore()).toEqual('Fifteen - Love');
-  }
+  });
 });
