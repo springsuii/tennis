d474937ae1455633a761fd699344b7dccf821458
Piyabut Nawanichbumrung <emunity3137@gmail.com>
2016-09-27 15:43:06 +0700

    test echo score A 15 - 0

--- spec/tennisAB.spec.js
+++ spec/tennisAB.spec.js
@@ -9,6 +9,7 @@ function tennis(){
     if(ScoreA === 0 && ScoreB === 0)
       return 'Love-Love';
   }
+
 }
 describe('Tennis', function () {
   it('Start Game score 0 - 0 echo Love - Love', function () {
@@ -16,4 +17,9 @@ describe('Tennis', function () {
     ten.startGame();
     expect(ten.echoScore()).toEqual('Love-Love');
   });
+  it('check score A 15 - B 0 echo fifteen - love',function()){
+    var ten = new tennis();
+    ten.scoreA();
+    expect(ten.echoScore()).toEqual('Fifteen - Love');
+  }
 });
